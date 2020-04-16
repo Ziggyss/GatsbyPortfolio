1e1060e4b8f83401eaf299c7203f82b566955980
Lisa Wilton <mishmao14@gmail.com>
2020-04-16 19:57:00 +0100

    logo and mobile nav working

---
+++ src/constants/links.js
@@ -0,0 +1,19 @@
+export default [
+
+    {
+        path: '/',
+        text: 'home'
+    },
+    {
+        path: '/projects',
+        text: 'projects'
+    },
+    {
+        path: '/about',
+        text: 'about'
+    },
+    {
+        path: '/contact',
+        text: 'contact'
+    },
+]
---
+++ src/constants/socialIcons.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import{FaFacebook, FaTwitterSquare, FaLinkedin} from 'react-icons/fa'
+
+export default [
+
+    {
+       icon: <FaFacebook/>,
+       url: 'https://facebook.com'
+    },
+    {
+        icon: <FaTwitterSquare/>,
+        url: 'https://twitter.com'
+     },
+     {
+        icon: <FaLinkedin/>,
+        url: 'https://linkedin.com'
+     },
+   
+]
