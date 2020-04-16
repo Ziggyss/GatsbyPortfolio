adde07d4f4e40d4c8a7fa6b511f4cac96339dd12
Lisa Wilton <mishmao14@gmail.com>
2020-04-16 20:22:51 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,41 @@
 import React from "react"
+import styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/socialIcons"
+import { Link } from "gatsby"
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer here</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, index) => {
+          return (
+            <Link key={index} to={item.path}>
+              {item.text}
+            </Link>
+          )
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
+        })}
+      </div>
+      <div className={styles.copyright}>
+        {" "}
+        copyright &copy; Lisa Wilton {new Date().getFullYear()} all rights
+        reserved
+      </div>
+    </footer>
   )
 }
 
