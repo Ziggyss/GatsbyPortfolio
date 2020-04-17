09a7b57a62b1c13927ff0f61333509a18c0eb548
Lisa Wilton <mishmao14@gmail.com>
2020-04-17 12:27:31 +0100

    some about stuff

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,12 +1,34 @@
 import React from "react"
 import StyledTitle from "../StyledTitle"
+import styles from "../../css/about.module.css"
+import img from "../../images/mowl.jpeg"
 
 const About = () => {
   return (
-    <div>
-      hello from about
+    <section className={styles.about}>
       <StyledTitle title="about" subtitle="me" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about me"></img>
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>blahblah blah</h4>
+          <p>
+            bladn gaoinasdoifna asldkf aslkfm asdfj aslkfn asldkf asdkf asldkfj
+            alskdjf aslkdjf.
+          </p>
+          <p>
+            bladn gaoinasdoifna asldkf aslkfm asdfj aslkfn asldkf asdkf asldkfj
+            alskdjf aslkdjf.
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
