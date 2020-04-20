34a35bd95665e7ace529f2dd179e218d642c5402
Lisa Wilton <mishmao14@gmail.com>
2020-04-20 16:34:14 +0100

    background image optimisation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Skills from "../components/Home/Skills"
-export default () => (
-  <>
-    <Layout>
-      <SimpleHero>
-        <Banner
-          title="Hello, I'm Lisa."
-          info="I'm a web developer from Belfast with a passion for creativity, design and nature. 
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
+export default ({ data }) => (
+  <Layout>
+    <StyledHero home="true" img={data.defaultHero.childImageSharp.fluid}>
+      <Banner
+        title="Hello, I'm Lisa."
+        info="I'm a web developer from Belfast with a passion for creativity, design and nature. 
            Welcome to my site. "
-        >
-          <Link to="/projects" className="btn-white">
-            Explore Projects
-          </Link>
-        </Banner>
-      </SimpleHero>
-      <About />
-      <Skills />
-    </Layout>
-  </>
+      >
+        <Link to="/projects" className="btn-white">
+          Explore Projects
+        </Link>
+      </Banner>
+    </StyledHero>
+    <About />
+    <Skills />
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultHero: file(relativePath: { eq: "Hero.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
