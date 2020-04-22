dc98f156dc18e5253eca2e8554bb3269dcbc5430
Lisa Wilton <mishmao14@gmail.com>
2020-04-22 14:36:31 +0100

    env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+}
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -19,7 +23,14 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
-
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
