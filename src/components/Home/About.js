618a4296acf0800ff86aba09b42d664c448db800
Lisa Wilton <mishmao14@gmail.com>
2021-07-22 14:45:56 +0100

    Update About.js

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -31,7 +31,7 @@ const About = () => {
         <article className={styles.aboutInfo}>
           <h4>Who Am I?</h4>
           <p>
-            I'm a creative, UX designer and writer with a love of nature, animals, photography and design. I am most passionate about creating beautiful web applications that can solve some of life's problems in a sustainable way.
+            I'm a creative UX designer and writer with a love of nature, animals, photography and design. I am most passionate about creating beautiful web applications that can solve some of life's problems in a sustainable way.
           </p>
           <p>
            In 2019 I completed a 10-month, fully remote course in full stack web development and computer science with <a href="www.lambdaschool.com">Lambda School,</a> learning a variety of new skills and technologies, including Javascript, React, NodeJs and PostgreSQL. More recently, I have discovered my love of UX design and UX writing and I've completed courses with IDF and the UX Writers' Collective to further my skills.
